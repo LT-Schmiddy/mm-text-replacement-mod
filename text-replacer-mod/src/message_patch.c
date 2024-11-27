b43113c29ea108e13170a52bda64a7d373c1cf1b
LT-Schmiddy <schmidalex1996@gmail.com>
2024-11-26 19:02:15 -0500

    patching Message_StartTextbox & Message_ContinueTextbox

--- text-replacer-mod/src/message_patch.c
+++ text-replacer-mod/src/message_patch.c
@@ -11,152 +11,63 @@ RECOMP_CALLBACK("*", recomp_on_init) void load_lib () {
 }
 
 void Message_FindMessage(PlayState* play, u16 textId);
+void Message_GrowTextbox(PlayState* play);
+void func_80150A84(PlayState* play);
 
-RECOMP_PATCH void Message_OpenText(PlayState* play, u16 textId) {
+RECOMP_PATCH void Message_StartTextbox(PlayState* play, u16 textId, Actor* actor) {
     MessageContext* msgCtx = &play->msgCtx;
     Font* font = &msgCtx->font;
-    Player* player = GET_PLAYER(play);
-    f32 var_fv0;
 
-    if (play->msgCtx.msgMode == MSGMODE_NONE) {
-        gSaveContext.prevHudVisibility = gSaveContext.hudVisibility;
-    }
-
-    if (textId == 0xFF) {
-        func_80115844(play, DO_ACTION_STOP);
-        play->msgCtx.hudVisibility = gSaveContext.hudVisibility;
-        Interface_SetHudVisibility(HUD_VISIBILITY_A_B_C);
-        gSaveContext.save.unk_06 = 20;
-    } else if ((textId == 0x579) || (textId == 0x8D8)) {
-        Interface_SetHudVisibility(HUD_VISIBILITY_A_HEARTS_MAGIC_WITH_OVERWRITE);
-    } else if (((textId == 0x28) || (textId == 0x29) || (textId == 0x2A)) &&
-               (player->transformation == PLAYER_FORM_DEKU)) {
-        //! FAKE:
-        if (msgCtx) {}
-        textId = 0xC9;
-    } else if (textId == 0x11) {
-        if (gSaveContext.save.saveInfo.inventory.strayFairies[((void)0, gSaveContext.dungeonIndex)] == 0xF) {
-            textId = 0xF3;
-        }
-    } else if ((textId == 0x92) && (play->sceneId == SCENE_KOEPONARACE)) {
-        textId = 0xCD;
-    }
-
-    if ((textId == 0xC) && (GET_QUEST_HEART_PIECE_COUNT != 0)) {
-        textId = GET_QUEST_HEART_PIECE_COUNT;
-        textId += 0xC4;
-    }
-
-    msgCtx->currentTextId = textId;
-
-    if ((msgCtx->currentTextId == 0xC) || (msgCtx->currentTextId == 0xD) || (msgCtx->currentTextId == 0xC5) ||
-        (msgCtx->currentTextId == 0xC6) || (msgCtx->currentTextId == 0xC7)) {
-        Interface_SetHudVisibility(HUD_VISIBILITY_A_HEARTS_MAGIC_WITH_OVERWRITE);
-    }
-
-    msgCtx->messageHasSetSfx = false;
-    D_801C6A70 = 0;
-    msgCtx->textboxSkipped = false;
-    msgCtx->textIsCredits = false;
-    var_fv0 = 1.0f;
-
-    if (play->pauseCtx.bombersNotebookOpen) {
-        if (gSaveContext.options.language == LANGUAGE_JPN) {
-            msgCtx->textCharScale = 1.4f;
-            msgCtx->unk11FFC = 0x1E;
-            msgCtx->unk11FF8 = 0x32;
-            var_fv0 = 1.4;
-        } else {
-            msgCtx->textCharScale = 1.4f;
-            msgCtx->unk11FFC = 0x16;
-            msgCtx->unk11FF8 = 0x32;
-            var_fv0 = 1.4;
-        }
-    } else if (textId >= 0x4E20) {
-        msgCtx->textIsCredits = true;
-        msgCtx->textCharScale = 0.85f;
-        msgCtx->unk11FFC = 6;
-        msgCtx->unk11FF8 = 0x14;
-    } else if (gSaveContext.options.language == LANGUAGE_JPN) {
-        msgCtx->textCharScale = 0.88f;
-        msgCtx->unk11FFC = 0x12;
-        msgCtx->unk11FF8 = 0x32;
-    } else {
-        msgCtx->textCharScale = 0.75f;
-        msgCtx->unk11FFC = 0xC;
-        msgCtx->unk11FF8 = 0x41;
-    }
-
-    sCharTexSize = msgCtx->textCharScale * 16.0f;
-    sCharTexScale = 1024.0f / msgCtx->textCharScale;
-    D_801F6B08 = 1024.0f / var_fv0;
-
- if (msgCtx->textIsCredits) {
-        Message_FindCreditsMessage(play, textId);
-        msgCtx->msgLength = font->messageEnd;
-        DmaMgr_SendRequest0(&font->msgBuf, (uintptr_t) &SEGMENT_ROM_START(staff_message_data_static)[font->messageStart],
-                            font->messageEnd);
-    } else if (gSaveContext.options.language == LANGUAGE_JPN) {
-        Message_FindMessage(play, textId);
-        msgCtx->msgLength = font->messageEnd;
-        DmaMgr_SendRequest0(&font->msgBuf, (uintptr_t) &SEGMENT_ROM_START(message_data_static)[font->messageStart],
-                            font->messageEnd);
-    } else {
-        Message_FindMessageNES(play, textId);
-        msgCtx->msgLength = font->messageEnd;
-        DmaMgr_SendRequest0(&font->msgBuf, (uintptr_t) &SEGMENT_ROM_START(message_data_static)[font->messageStart],
-                            font->messageEnd);
-    }
+    msgCtx->ocarinaAction = 0xFFFF;
+    Message_OpenText(play, textId);
 
     for (int i = 0; i < msgCtx->msgLength; i++) {
         text_replacer_set_char_in_buffer(font->msgBuf.schar[i], i);
-        recomp_printf("%c", font->msgBuf.schar[i]);
+        // recomp_printf("%c", font->msgBuf.schar[i]);
     }
 
     text_replacer_add_buffer_as_entry(textId, msgCtx->msgLength);
 
-    msgCtx->choiceNum = 0;
-    msgCtx->textUnskippable = false;
-    msgCtx->textboxEndType = TEXTBOX_ENDTYPE_00;
-    msgCtx->textDrawPos = 0;
-    msgCtx->msgBufPos = 0;
-    msgCtx->decodedTextLen = 0;
+    msgCtx->talkActor = actor;
+    msgCtx->msgMode = MSGMODE_TEXT_START;
+    msgCtx->stateTimer = 0;
+    msgCtx->textDelayTimer = 0;
+    play->msgCtx.ocarinaMode = OCARINA_MODE_NONE;
+}
 
-    msgCtx->unk11F08 = font->msgBuf.schar[msgCtx->msgBufPos] << 8;
-    msgCtx->unk11F08 |= font->msgBuf.schar[msgCtx->msgBufPos + 1];
+RECOMP_PATCH void Message_ContinueTextbox(PlayState* play, u16 textId) {
+    MessageContext* msgCtx = &play->msgCtx;
+    InterfaceContext* interfaceCtx = &play->interfaceCtx;
+    Font* font = &msgCtx->font;
 
-    msgCtx->unk11F18 = (msgCtx->unk11F08 & 0xF000) >> 0xC;
-    msgCtx->textBoxType = (msgCtx->unk11F08 & 0xF00) >> 8;
-    msgCtx->textBoxPos = (msgCtx->unk11F08 & 0xF0) >> 4;
-    msgCtx->unk11F0C = msgCtx->unk11F08 & 0xF;
+    msgCtx->msgLength = 0;
+    Message_OpenText(play, textId);
+    func_80150A84(play);
 
-    if ((msgCtx->unk11F0C == 1) || (msgCtx->unk11F0C == 3)) {
-        msgCtx->textUnskippable = true;
-    }
-    msgCtx->itemId = 0xFE;
-
-    if ((msgCtx->textBoxType == TEXTBOX_TYPE_5) || (msgCtx->textBoxType == TEXTBOX_TYPE_D) ||
-        (play->pauseCtx.bombersNotebookOpen)) {
-        msgCtx->unk120CE = msgCtx->unk120D0 = msgCtx->unk120D2 = 0;
-    } else {
-        msgCtx->unk120CE = msgCtx->unk120D0 = msgCtx->unk120D2 = 0xFF;
+    for (int i = 0; i < msgCtx->msgLength; i++) {
+        text_replacer_set_char_in_buffer(font->msgBuf.schar[i], i);
+        // recomp_printf("%c", font->msgBuf.schar[i]);
     }
 
-    msgCtx->choiceIndex = 0;
+    text_replacer_add_buffer_as_entry(textId, msgCtx->msgLength);
 
-    if (msgCtx->unk11F0C != 3) {
-        msgCtx->textColorAlpha = 0xFF;
-    } else {
-        msgCtx->textColorAlpha = 0;
+    msgCtx->msgMode = MSGMODE_TEXT_CONTINUING;
+    msgCtx->stateTimer = 8;
+    msgCtx->textDelayTimer = 0;
+
+    if (interfaceCtx->unk_222 == 0) {
+        if (textId != 0x1B93) {
+            func_8011552C(play, DO_ACTION_NEXT);
+        } else if (textId != 0xF8) {
+            func_8011552C(play, DO_ACTION_DECIDE);
+        }
     }
+    msgCtx->textboxColorAlphaCurrent = msgCtx->textboxColorAlphaTarget;
 
-    if (textId == 0x1467) {
-        Message_BombersNotebookQueueEvent(play, BOMBERS_NOTEBOOK_EVENT_MET_KAFEI);
-    }
-
-    if (textId == 0x2955) {
-        Message_BombersNotebookQueueEvent(play, BOMBERS_NOTEBOOK_EVENT_MET_ANJU);
-        Message_BombersNotebookQueueEvent(play, BOMBERS_NOTEBOOK_EVENT_MET_KAFEI);
-        Message_BombersNotebookQueueEvent(play, BOMBERS_NOTEBOOK_EVENT_RECEIVED_COUPLES_MASK);
+    if (play->pauseCtx.bombersNotebookOpen) {
+        msgCtx->textboxXTarget = 34;
+        msgCtx->textboxYTarget = 350;
+        Message_GrowTextbox(play);
+        msgCtx->stateTimer = 1;
     }
 }
